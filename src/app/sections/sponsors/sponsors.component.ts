9e019543182590f7de9561bc70cb7f71bb697cb7
Essimbi <essimbideranot@gmail.com>
2026-06-30 12:10:54 +0100

    Ajout des partenaires (Konnex, Platinum Cocotier, MINPOSTEL, DMS, Balafon TV)

--- src/app/sections/sponsors/sponsors.component.ts
+++ src/app/sections/sponsors/sponsors.component.ts
@@ -14,14 +14,17 @@ export class SponsorsComponent implements AfterViewInit {
     { name: 'Port Autonome de Douala', logo: '/assets/partners/port_autonome_de_douala.png' },
     { name: 'Direction Générale des Douanes', logo: '/assets/partners/direction_generale_des_douanes.jpeg' },
     { name: 'Ministère de la Promotion de la Femme et de la Famille', logo: '/assets/partners/minister_de_la_formation_professionnelle_dej_la_femme_et_de_la_famille.png' },
+    { name: 'Ministère des Postes et Télécommunications', logo: '/assets/partners/ministere_des_posts_et_telecom.jpeg' },
     { name: 'Terminal Bois du Port de Douala', logo: '/assets/partners/terminal_bois_du_port_de_douala.png' },
     { name: 'Agora Shipping & Logistics', logo: '/assets/partners/agora_shipping_logistics.jpeg' },
     { name: 'Maersk', logo: '/assets/partners/maersk_group.jpg' },
+    { name: 'Konnex', logo: '/assets/partners/konnex.jpeg' },
+    { name: 'Destiny Marine Survey', logo: '/assets/partners/destiny_marine_survey.jpeg' },
     { name: '3CM Communications & Technologies', logo: '/assets/partners/3cm_communications_et_technologies.jpeg' },
+    { name: 'Balafon TV', logo: '/assets/partners/balafon_tv.jpeg' },
     { name: 'Projecteur Magazine', logo: '/assets/partners/projecteur_magazine.png' },
-    // { name: 'Ministère des Postes et Télécommunications', logo: '/assets/partners/ministere_des_posts_et_telecom.jpeg' },
     // { name: 'Ministère des Transports', logo: '/assets/partners/ministere_des_transport.jpg' },
-    // { name: 'Platinum Cocotiers Hôtel', logo: '/assets/partners/platinum_cocotiers_hotel.jpg' },
+    { name: 'Platinum Cocotiers Hôtel', logo: '/assets/partners/platinum_cocotiers_hotel.jpg' },
   ];
 
   constructor(private sa: ScrollAnimationService) {}
